718629683d6f5e846feca5a13258ef68116b11e6
zeuz <zeuz@zeuzs-iMac.local>
2023-05-16 15:24:04 -0400

    set up api route for retreiving notes

---
+++ routes/apiRoutes.js
@@ -0,0 +1,26 @@
+const router = require("express").Router();
+const { v4: uuidv4 } = require("uuid");
+const {
+  readFromFile,
+  readAndAppend,  
+  readAndDelete,
+} = require("../utils/helpers");
+
+router.get("/notes", (req, res) => {
+  readFromFile("./db/db.json").then((data) => res.json(JSON.parse(data)));
+});
+
+router.post("/notes", (req, res) => {
+  const newNote = req.body;
+  newNote.id = uuidv4();
+
+  readAndAppend(newNote, "./db/db.json");
+  res.json(newNote);
+});
+
+router.delete("/notes/:id", (req, res) => {
+  readAndDelete(req.params.id, "./db/db.json");
+  res.json({ ok: true });
+});
+
+module.exports = router;
