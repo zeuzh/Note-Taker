cf1bd499fb30820b8053fdfea2d24961775f680d
zeuz <zeuz@zeuzs-iMac.local>
2023-06-20 15:50:05 -0400

    little fixes in paths for htmlRoutes

--- server.js
+++ server.js
@@ -6,8 +6,8 @@ const PORT = process.env.PORT || 3001;
 const apiRoutes = require('./routes/apiRoutes');
 const homeRoutes = require('./routes/homeRoutes');
 
-app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 app.use(express.static('public'));
 
 app.use('/', homeRoutes);
